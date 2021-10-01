20e8456bcd105420fdd203b15633abb676836299
Grigoriy Efimov <efimov-gm@newpage.xyz>
2021-10-02 01:33:33 +0300

    Add get_users #6

--- sql/stored-procedure.sql
+++ sql/stored-procedure.sql
@@ -21,3 +21,26 @@ begin
 `users`.`birth_date` FROM `users` WHERE `users`.`id`=UID;
 end;
 //
+
+create procedure get_users(
+    IN TYPE_select INT UNSIGNED
+)
+begin
+ IF TYPE_select<3
+    THEN 
+        SELECT `users`.`id`,`users`.`email`,`users`.`name`,`users`.`surname`,`users`.`patronymic`,`users`.`full_name`,`users`.`post`,`users`.`contact_type`,
+        `users`.`is_supervisor`,`users`.`vacation_confirm`,`users`.`phone`,`users`.`emerge_phone`,`users`.`spent_time`,`users`.`role`,
+        `users`.`remote`,`users`.`skype`,`users`.`telegram`,`users`.`office`,`users`.`sub_company`,`users`.`entity`,`users`.`status`,`users`.`work_start`,
+        `users`.`work_start_here`,`users`.`last_change_name`,`users`.`last_change_date`,`users`.`sex`,`users`.`login_lock`,
+        `users`.`emoji_status`,`users`.`user_status`,`users`.`avatar`,`users`.`cv`,`users`.`vacation_start`,`users`.`vacation_end`,
+        `users`.`birth_date` FROM `users` WHERE `users`.`role`=TYPE_select;
+    ELSE
+    SELECT `users`.`id`,`users`.`email`,`users`.`name`,`users`.`surname`,`users`.`patronymic`,`users`.`full_name`,`users`.`post`,`users`.`contact_type`,
+    `users`.`is_supervisor`,`users`.`vacation_confirm`,`users`.`phone`,`users`.`emerge_phone`,`users`.`spent_time`,`users`.`role`,
+    `users`.`remote`,`users`.`skype`,`users`.`telegram`,`users`.`office`,`users`.`sub_company`,`users`.`entity`,`users`.`status`,`users`.`work_start`,
+    `users`.`work_start_here`,`users`.`last_change_name`,`users`.`last_change_date`,`users`.`sex`,`users`.`login_lock`,
+    `users`.`emoji_status`,`users`.`user_status`,`users`.`avatar`,`users`.`cv`,`users`.`vacation_start`,`users`.`vacation_end`,
+    `users`.`birth_date` FROM `users`;
+    END IF;
+end;
+//
