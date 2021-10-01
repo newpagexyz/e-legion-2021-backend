2bba7fbf9289b1c1460aef3b28064d6b11b2e320
Grigoriy Efimov <efimov-gm@newpage.xyz>
2021-10-01 23:14:13 +0300

    Add get_user_info stored procedure #4

--- sql/stored-procedure.sql
+++ sql/stored-procedure.sql
@@ -1,1 +1,27 @@
 use `elegion_db`;  
+/*
+    Хранимые процедуры
+*/
+delimiter //
+
+
+/*
+    Процедура получения информации о пользователе
+*/
+create procedure get_user_info(
+    IN `UID` INT UNSIGNED
+)
+begin
+  SELECT `users`.`id`,`users`.`email`,`users`.`name`,`users`.`surname`,`users`.`patronymic`,`users`.`full_name`,`users`.`post`,`users`.`contact_type`,
+`users`.`is_supervisor`,`users`.`vacation_confirm`,`users`.`phone`,`users`.`emerge_phone`,`users`.`spent_time`,`users`.`role`,
+`users`.`remote`,`users`.`skype`,`users`.`telegram`,`users`.`office`,`users`.`sub_company`,`users`.`entity`,`users`.`status`,`users`.`work_start`,
+`users`.`work_start_here`,`users`.`last_change_name`,`users`.`last_change_date`,`users`.`sex`,`users`.`login_lock`,
+`users`.`emoji_status`,`users`.`user_status`,`users`.`avatar`,`users`.`cv`,`users`.`vacation_start`,`users`.`vacation_end`,
+`users`.`birth_date` FROM `users` WHERE `users`.`id`=UID;
+end;
+//
+
+
+
+
+delimiter ;
